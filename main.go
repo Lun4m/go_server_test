b9bd6f4cc233da6421536b04960bb7aaa0ca74f4
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-04-03 23:03:52 +0200

    Serve on api path

--- main.go
+++ main.go
@@ -61,9 +61,9 @@ func main() {
 
 	mux.Handle("/app/*", config.middlewareMetricsInc(baseHandler))
 
-	mux.HandleFunc("GET /healthz", healthHandler)
-	mux.HandleFunc("GET /metrics", config.metricsHandler)
-	mux.HandleFunc("GET /reset", config.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("GET /api/metrics", config.metricsHandler)
+	mux.HandleFunc("GET /api/reset", config.resetHandler)
 
 	corsMux := middlewareCors(mux)
 	server := &http.Server{Addr: ":" + port, Handler: corsMux}
